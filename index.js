d3f97607460d53e255be69142b756bcf5fc6c7f2
Usha Sai Chintha <ushasai154@gmail.com>
2023-06-13 20:32:07 +0530

    movies graphql server setup

---
+++ index.js
@@ -0,0 +1,23 @@
+let movies = require('./data/movies')
+const genres = require('./data/genres')
+
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello World"
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({url}) => {
+    console.log(`Server is running at ${url}`)
+})
+
